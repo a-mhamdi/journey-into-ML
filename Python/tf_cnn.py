cfad842946e375d5135ffcbdb273b5796a48408a
a-mhamdi <a_mhamdi@outlook.com>
2023-08-12 00:44:08 +0100

    update data location

--- Python/tf_cnn.py
+++ Python/tf_cnn.py
@@ -10,7 +10,7 @@ from tqdm import trange
 
 from tensorflow.examples.tutorials.mnist import input_data
 # Import MNIST dataset
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../Datasets/MNIST_data/", one_hot=True)
 
 X_test = mnist.test.images.reshape(-1, 28, 28, 1)
 y_test = mnist.test.labels
