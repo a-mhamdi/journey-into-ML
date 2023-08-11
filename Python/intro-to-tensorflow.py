57a6849fe509fde7e2cc9f2bb974a3ecb1f5017d
a-mhamdi <a_mhamdi@outlook.com>
2023-08-11 22:42:35 +0100

    GPU support (NOT WORKING)

--- Python/intro-to-tensorflow.py
+++ Python/intro-to-tensorflow.py
@@ -1,4 +1,14 @@
+#= conda activate introML =# `tf__version__ = 1.14.0`  
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
+
+"""
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+"""
 
 # tf.reset_default_graph()
 
