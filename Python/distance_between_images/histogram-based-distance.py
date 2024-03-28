f99df50239eba84559010f5d413426a233754989
a-mhamdi <a_mhamdi@outlook.com>
2024-03-28 15:47:35 +0100

    histogram w/ info on pixel position

--- Python/distance_between_images/histogram-based-distance.py
+++ Python/distance_between_images/histogram-based-distance.py
@@ -1,5 +1,7 @@
 # %% Import the required modules
 import numpy as np
+from math import floor
+
 from matplotlib import pyplot as plt
 
 from loader import load_images
@@ -8,6 +10,7 @@ from loader import load_images
 lst_images = ['1', '3']
 img1, img2 = load_images('image-'+lst_images[0]+'.png', 'image-'+lst_images[1]+'.png')
 img = np.concatenate((img1[:, :, :, np.newaxis], img2[:, :, :, np.newaxis]), axis=3)
+
 # %% Calculate the histogram-based distance between the two images
 hist = np.zeros((256, 3, 2), dtype=np.uint64)
 # Tuple to select colors of each channel line
@@ -19,7 +22,6 @@ for i in range(0, 2):
         hist[:, channel_id, i], bin_edges = np.histogram(
             img[:, :, channel_id, i], bins=256, range=(0, 256)
         )
-
 
 # %% Plot the histograms
 fig = plt.figure(layout="constrained")
@@ -48,3 +50,37 @@ for i, loc in enumerate(['d', 'e']):
     ax[loc].imshow(image)
 
 plt.show()
+
+# %% Histogram of multiple images
+colors = ('red', 'green', 'blue')
+img1, img2 = load_images('image-1.png', 'image-3.png')
+width, height, _ = img.shape
+pas, nbins = 32, 256
+
+# %%
+data_hist = np.zeros((nbins*(floor(width/pas)-1), 3, 2), dtype=np.uint64)
+for i in range(0, 2):
+    img = img1 if i == 0 else img2
+    for channel_id, color in enumerate(colors):
+        hist = [[], [], []]
+        debut, fin = 0, pas
+        while True:
+            img_trunc = img[debut:fin, :, channel_id]
+            debut = fin
+            fin += pas
+            if fin > width:
+                break
+            tmp, bin_edges = np.histogram(
+                img_trunc, bins=nbins, range=(0, nbins)
+                )
+            hist[channel_id].extend(list(tmp))
+        data_hist[:, channel_id, i] = np.array(hist[channel_id])
+# %%
+_, ax = plt.subplots(3, 1)
+cosine_sim = {}
+for i in range(0, 3):
+    ax[i].plot(data_hist[:, i, :], color=colors[i])
+    cosine_sim[colors[i]] = np.dot(data_hist[:, i, 0], data_hist[:, i, 1]) / (np.linalg.norm(data_hist[:, i, 0]) * np.linalg.norm(data_hist[:, i, 1]))
+    print(f'{colors[i]}: {cosine_sim[colors[i]]}')
+
+# %%
